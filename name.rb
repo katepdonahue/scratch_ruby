07c2cf821ecd6a19212af07168359ed8b13279b8
Kate <katepdonahue@gmail.com>
2013-10-25 11:29:04 -0400

    A short program variation on example in Chris Pine's Learn to Program.

---
+++ name.rb
@@ -0,0 +1,8 @@
+puts "Hello there, what's your name?"
+name = gets.chomp
+print "Your name is #{name}? I used to know a #{name}..."
+sleep 2
+puts "sigh"
+sleep 1
+puts "Well, nice to meet you, #{name}."
+
